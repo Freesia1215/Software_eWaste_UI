8bfb8a6d1f11f38aaafda95854ce856c8d809eaf
PsychoMaple <92438827+PsychoMaple@users.noreply.github.com>
2022-03-25 20:02:52 +0000

    own-device

--- project/routes/index.js
+++ project/routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/owndevice',function(req,res,next){
+  res.render('owndevice',{title:'owndevice'})
+})
 
 module.exports = router;
